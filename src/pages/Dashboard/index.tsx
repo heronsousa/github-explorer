eedaeb50849474f3f7f1b73b609246776fb175bc
Heron Rodrigues <heronrs1@gmail.com>
2021-01-12 00:33:28 -0300

    Adding styles to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
     return (
-        <Title>Explore repositórios no GitHub</Title>
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no GitHub</Title>
+
+            <Form action="">
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="ola">
+                    <img src="" alt="Heron Rodrigues" />
+
+                    <div>
+                        <strong>marvel-heroes</strong>
+                        <p>Aplicativo para visualizar heróis da Marvel</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="ola">
+                    <img src="" alt="Heron Rodrigues" />
+
+                    <div>
+                        <strong>marvel-heroes</strong>
+                        <p>Aplicativo para visualizar heróis da Marvel</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="ola">
+                    <img src="" alt="Heron Rodrigues" />
+
+                    <div>
+                        <strong>marvel-heroes</strong>
+                        <p>Aplicativo para visualizar heróis da Marvel</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
     );
-}
+};
 
 export default Dashboard;
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Respository: React.FC = () => {
-    return (
-        <h1> Respository</h1>
-    );
-}
+    return <h1> Respository</h1>;
+};
 
 export default Respository;
