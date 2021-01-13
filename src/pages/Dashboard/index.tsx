f5e2407678bfad1ca091b2f8e3769674d6de4231
Heron Rodrigues <heronrs1@gmail.com>
2021-01-12 21:51:53 -0300

    Connecting to github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,51 +1,67 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
 const Dashboard: React.FC = () => {
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [newRepo, setNewRepo] = useState('');
+
+    async function handleAddRepository(
+        event: FormEvent<HTMLFormElement>
+    ): Promise<void> {
+        event.preventDefault();
+
+        const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+        setRepositories([...repositories, response.data]);
+        setNewRepo('');
+    }
+
     return (
         <>
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no GitHub</Title>
 
-            <Form action="">
-                <input placeholder="Digite o nome do repositório" />
+            <Form onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={(e) => {
+                        setNewRepo(e.target.value);
+                    }}
+                    placeholder="Digite o nome do repositório"
+                />
                 <button type="submit">Pesquisar</button>
             </Form>
 
             <Repositories>
-                <a href="ola">
-                    <img src="" alt="Heron Rodrigues" />
+                {repositories.map((repository) => (
+                    <a key={repository.full_name} href="ola">
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
 
-                    <div>
-                        <strong>marvel-heroes</strong>
-                        <p>Aplicativo para visualizar heróis da Marvel</p>
-                    </div>
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
 
-                    <FiChevronRight size={20} />
-                </a>
-                <a href="ola">
-                    <img src="" alt="Heron Rodrigues" />
-
-                    <div>
-                        <strong>marvel-heroes</strong>
-                        <p>Aplicativo para visualizar heróis da Marvel</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
-                <a href="ola">
-                    <img src="" alt="Heron Rodrigues" />
-
-                    <div>
-                        <strong>marvel-heroes</strong>
-                        <p>Aplicativo para visualizar heróis da Marvel</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
             </Repositories>
         </>
     );
