123c85af80dba75bd0b42729a4c28b9861581db8
Heron Rodrigues <heronrs1@gmail.com>
2021-01-16 21:48:08 -0300

    Styling repository page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
     full_name: string;
@@ -16,18 +17,43 @@ interface Repository {
 }
 
 const Dashboard: React.FC = () => {
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem(
+            '@GithubExplorer:repositories'
+        );
+
+        if (storagedRepositories) return JSON.parse(storagedRepositories);
+
+        return [];
+    });
     const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+
+    useEffect(() => {
+        localStorage.setItem(
+            '@GithubExplorer:repositories',
+            JSON.stringify(repositories)
+        );
+    }, [repositories]);
 
     async function handleAddRepository(
         event: FormEvent<HTMLFormElement>
     ): Promise<void> {
         event.preventDefault();
+        if (!newRepo) {
+            setInputError('Insira o autor/nome do repositório');
+            return;
+        }
 
-        const response = await api.get<Repository>(`/repos/${newRepo}`);
+        try {
+            const response = await api.get<Repository>(`/repos/${newRepo}`);
 
-        setRepositories([...repositories, response.data]);
-        setNewRepo('');
+            setRepositories([...repositories, response.data]);
+            setNewRepo('');
+            setInputError('');
+        } catch (error) {
+            setInputError('Erro na busca por esse repositório');
+        }
     }
 
     return (
@@ -35,7 +61,7 @@ const Dashboard: React.FC = () => {
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no GitHub</Title>
 
-            <Form onSubmit={handleAddRepository}>
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
                 <input
                     value={newRepo}
                     onChange={(e) => {
@@ -46,9 +72,14 @@ const Dashboard: React.FC = () => {
                 <button type="submit">Pesquisar</button>
             </Form>
 
+            {inputError && <Error>{inputError}</Error>}
+
             <Repositories>
                 {repositories.map((repository) => (
-                    <a key={repository.full_name} href="ola">
+                    <Link
+                        key={repository.full_name}
+                        to={`/repository/${repository.full_name}`}
+                    >
                         <img
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login}
@@ -60,7 +91,7 @@ const Dashboard: React.FC = () => {
                         </div>
 
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link, useRouteMatch } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+import { Header, RespositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+    repository: string;
+}
 
 const Respository: React.FC = () => {
-    return <h1> Respository</h1>;
+    useEffect(() => {
+        async function getRepo() {
+            await api.get(`repos/${params.repository}`).then((response) => {
+                console.log(response.data)
+            });
+
+        }
+    }, []);
+
+    const { params } = useRouteMatch<RepositoryParams>();
+
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RespositoryInfo>
+                <header>
+                    <img alt="Imagem" />
+                    <div>
+                        <strong>facebook/react</strong>
+                        <p>Descrição do repositório</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RespositoryInfo>
+
+            <Issues>
+                <Link to="asdsad">
+                    <div>
+                        <strong>repository.full_name</strong>
+                        <p>repository.description</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
 };
 
 export default Respository;
